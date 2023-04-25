8a835a6594ffca2827c4b6ed84bcc13b85779858
Andhika <moore.dhikaz@gmail.com>
2023-04-25 18:10:25 +0700

    add alta icon button and alta scaffold

--- lib/feature/auth/forgot_password/view/ui/forgot_password_page_2.dart
+++ lib/feature/auth/forgot_password/view/ui/forgot_password_page_2.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../../../../utils/widgets/alta_scaffold.dart';
 import 'forgot_password_page_3.dart';
 
 import '../../../login/view/ui/login_page.dart';
@@ -45,115 +46,102 @@ class _ForgotPasswordPage2State extends State<ForgotPasswordPage2> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: AltaColor.white,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          leading: IconButton(
-            color: AltaColor.black,
-            icon: SvgPicture.asset(
-                'assets/icon/login_section/svg/arrow_back_icon.svg'),
-            iconSize: 14,
-            onPressed: () =>
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-              builder: (context) => const LoginPage(),
-            )),
-          ),
+    return AltaScaffold(
+      leadingAsset: 'assets/icon/login_section/svg/arrow_back_icon.svg',
+      leadingHeight: 17,
+      leadingWidth: 15,
+      onPressed: () => Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const LoginPage())),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          left: AltaSpacing.space16,
+          right: AltaSpacing.space16,
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(
-            left: AltaSpacing.space16,
-            right: AltaSpacing.space16,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: AltaSpacing.space56),
-              AltaText(
-                context: context,
-                text: 'Langkah 2/2',
-                style: AltaTextStyle.titleH2,
-                color: AltaColor.black,
-              ),
-              const SizedBox(height: AltaSpacing.space16),
-              AltaText(
-                context: context,
-                text:
-                    'Klik tautan lupa kata sandi yang telah kami kirim ke ${widget.email}',
-                style: AltaTextStyle.headlineH1,
-                color: AltaColor.black,
-              ),
-              const SizedBox(height: AltaSpacing.space24),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  AltaText(
-                    context: context,
-                    text: 'Email yang dimasukkan salah?',
-                    style: AltaTextStyle.titleH2,
-                    color: AltaColor.darkGray,
-                  ),
-                  const SizedBox(width: 8),
-                  AltaTextButton(
-                    onPressed: () {},
-                    child: AltaText(
-                      context: context,
-                      text: 'Ubah Email',
-                      style: AltaTextStyle.titleH2,
-                      color: AltaColor.tangerine,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: AltaSpacing.space8),
-              ValueListenableBuilder(
-                valueListenable: seconds,
-                builder: (BuildContext context, secondsValue, Widget? child) =>
-                    AltaText(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: AltaSpacing.space56),
+            AltaText(
+              context: context,
+              text: 'Langkah 2/2',
+              style: AltaTextStyle.titleH2,
+              color: AltaColor.black,
+            ),
+            const SizedBox(height: AltaSpacing.space16),
+            AltaText(
+              context: context,
+              text:
+                  'Klik tautan lupa kata sandi yang telah kami kirim ke ${widget.email}',
+              style: AltaTextStyle.headlineH1,
+              color: AltaColor.black,
+            ),
+            const SizedBox(height: AltaSpacing.space24),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                AltaText(
                   context: context,
-                  text:
-                      'Belum menerima tautan verifikasi? Kirim ulang dalam ${secondsValue.toString()}',
+                  text: 'Email yang dimasukkan salah?',
                   style: AltaTextStyle.titleH2,
                   color: AltaColor.darkGray,
                 ),
+                const SizedBox(width: 8),
+                AltaTextButton(
+                  onPressed: () {},
+                  child: AltaText(
+                    context: context,
+                    text: 'Ubah Email',
+                    style: AltaTextStyle.titleH2,
+                    color: AltaColor.tangerine,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: AltaSpacing.space8),
+            ValueListenableBuilder(
+              valueListenable: seconds,
+              builder: (BuildContext context, secondsValue, Widget? child) =>
+                  AltaText(
+                context: context,
+                text:
+                    'Belum menerima tautan verifikasi? Kirim ulang dalam ${secondsValue.toString()}',
+                style: AltaTextStyle.titleH2,
+                color: AltaColor.darkGray,
               ),
-              const SizedBox(height: AltaSpacing.space24),
-              Row(
-                children: [
-                  Expanded(
-                    child: ValueListenableBuilder(
-                      valueListenable: seconds,
-                      builder: (BuildContext context, secondsValue, child) =>
-                          AltaPrimaryButton(
-                        backgroundColor: MaterialStateProperty.resolveWith(
-                          (states) => secondsValue == 0
-                              ? AltaColor.darkBlue
-                              : AltaColor.altGray2,
-                        ),
-                        borderRadius: AltaBorderRadius.radius8,
-                        paddingVertical: AltaSpacing.space20,
-                        paddingHorizontal: AltaSpacing.space28,
-                        onPressed: () => secondsValue == 0
-                            ? Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) =>
-                                    const ForgotPasswordPage3(),
-                              ))
-                            : null,
-                        child: AltaText(
-                          context: context,
-                          text: 'KIRIM ULANG TAUTAN',
-                          style: AltaTextStyle.titleH1,
-                          color: AltaColor.white,
-                        ),
+            ),
+            const SizedBox(height: AltaSpacing.space24),
+            Row(
+              children: [
+                Expanded(
+                  child: ValueListenableBuilder(
+                    valueListenable: seconds,
+                    builder: (BuildContext context, secondsValue, child) =>
+                        AltaPrimaryButton(
+                      backgroundColor: MaterialStateProperty.resolveWith(
+                        (states) => secondsValue == 0
+                            ? AltaColor.darkBlue
+                            : AltaColor.altGray2,
+                      ),
+                      borderRadius: AltaBorderRadius.radius8,
+                      paddingVertical: AltaSpacing.space20,
+                      paddingHorizontal: AltaSpacing.space28,
+                      onPressed: () => secondsValue == 0
+                          ? Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const ForgotPasswordPage3(),
+                            ))
+                          : null,
+                      child: AltaText(
+                        context: context,
+                        text: 'KIRIM ULANG TAUTAN',
+                        style: AltaTextStyle.titleH1,
+                        color: AltaColor.white,
                       ),
                     ),
                   ),
-                ],
-              )
-            ],
-          ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
--- lib/feature/auth/forgot_password/view/ui/forgot_password_page_3.dart
+++ lib/feature/auth/forgot_password/view/ui/forgot_password_page_3.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_alterra_app/utils/widgets/alta_icon_button.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+import '../../../../../utils/widgets/alta_scaffold.dart';
 import 'confirm_new_password_page.dart';
 
 import '../../../../../utils/widgets/alta_text_button.dart';
@@ -28,205 +30,203 @@ class ForgotPasswordPage3 extends StatelessWidget {
       }
     }
 
-    return Container(
-      color: AltaColor.white,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          leading: IconButton(
-            color: AltaColor.black,
-            icon: SvgPicture.asset(
-                'assets/icon/login_section/svg/arrow_back_icon.svg'),
-            iconSize: 14,
-            onPressed: () => Navigator.of(context).pop(true),
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: SvgPicture.asset(
-                  'assets/icon/login_section/svg/move_icon.svg'),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: SvgPicture.asset(
-                  'assets/icon/login_section/svg/trash_icon.svg'),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: SvgPicture.asset(
-                  'assets/icon/login_section/svg/email_icon.svg'),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: SvgPicture.asset(
-                  'assets/icon/login_section/svg/more_vert_icon.svg'),
-            ),
-          ],
+    return AltaScaffold(
+      leadingAsset: 'assets/icon/login_section/svg/arrow_back_icon.svg',
+      leadingHeight: 17,
+      leadingWidth: 15,
+      onPressed: () {},
+      actions: const [
+        AltaIconButton(
+          svgAsset: 'assets/icon/login_section/svg/move_icon.svg',
+          iconWidth: 17,
+          iconHeight: 16,
         ),
-        body: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Padding(
-            padding: const EdgeInsets.only(
-              left: AltaSpacing.space16,
-              right: AltaSpacing.space16,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: AltaSpacing.space28),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        AltaIconButton(
+          svgAsset: 'assets/icon/login_section/svg/trash_icon.svg',
+          iconWidth: 15,
+          iconHeight: 18,
+        ),
+        AltaIconButton(
+          svgAsset: 'assets/icon/login_section/svg/email_icon.svg',
+          iconWidth: 17,
+          iconHeight: 13.6,
+        ),
+        AltaIconButton(
+          svgAsset: 'assets/icon/login_section/svg/more_vert_icon.svg',
+          iconWidth: 3.46,
+          iconHeight: 15,
+        ),
+      ],
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: AltaSpacing.space16,
+            right: AltaSpacing.space16,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: AltaSpacing.space28),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  AltaText(
+                    context: context,
+                    text:
+                        'Verifikasi email Anda untuk \nmerubah kata sandi \nAlterra Academy',
+                    style: AltaTextStyle.headlineH2,
+                    color: AltaColor.black,
+                    textAlign: TextAlign.justify,
+                  ),
+                  const SizedBox(width: AltaSpacing.space4),
+                  SvgPicture.asset(
+                      'assets/icon/login_section/svg/starred_icon.svg'),
+                ],
+              ),
+              const SizedBox(height: AltaSpacing.space32),
+              ListTile(
+                leading: const CircleAvatar(
+                  backgroundColor: AltaColor.darkBlue,
+                ),
+                title: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     AltaText(
                       context: context,
-                      text:
-                          'Verifikasi email Anda untuk \nmerubah kata sandi \nAlterra Academy',
-                      style: AltaTextStyle.headlineH2,
-                      color: AltaColor.black,
-                      textAlign: TextAlign.justify,
+                      text: 'Alterra Admin',
+                      style: AltaTextStyle.titleH2,
+                      color: AltaColor.darkGray,
                     ),
-                    const SizedBox(width: AltaSpacing.space4),
-                    SvgPicture.asset(
-                        'assets/icon/login_section/svg/starred_icon.svg'),
+                    const SizedBox(width: AltaSpacing.space8),
+                    AltaText(
+                      context: context,
+                      text: 'May 6',
+                      style: AltaTextStyle.bodyH2,
+                      color: AltaColor.darkGray,
+                    ),
                   ],
                 ),
-                const SizedBox(height: AltaSpacing.space32),
-                ListTile(
-                  leading: const CircleAvatar(
-                    backgroundColor: AltaColor.darkBlue,
-                  ),
-                  title: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      AltaText(
-                        context: context,
-                        text: 'Alterra Admin',
-                        style: AltaTextStyle.titleH2,
-                        color: AltaColor.darkGray,
-                      ),
-                      const SizedBox(width: AltaSpacing.space8),
-                      AltaText(
-                        context: context,
-                        text: 'May 6',
-                        style: AltaTextStyle.bodyH2,
-                        color: AltaColor.darkGray,
-                      ),
-                    ],
-                  ),
-                  subtitle: Row(
-                    children: [
-                      AltaText(
-                        context: context,
-                        text: 'to me',
-                        style: AltaTextStyle.bodyH1,
-                        color: AltaColor.darkGray,
-                      ),
-                      const SizedBox(width: AltaSpacing.space8),
-                      SvgPicture.asset(
-                          'assets/icon/login_section/svg/vert_down_icon.svg'),
-                    ],
-                  ),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      IconButton(
-                        padding: EdgeInsets.zero,
-                        onPressed: () {},
-                        icon: SvgPicture.asset(
-                            'assets/icon/login_section/svg/reply_icon.svg'),
-                      ),
-                      IconButton(
-                        padding: EdgeInsets.zero,
-                        onPressed: () {},
-                        icon: SvgPicture.asset(
-                            'assets/icon/login_section/svg/more_vert_icon.svg'),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: AltaSpacing.space8),
-                const AltaLogo(
-                  imgPath:
-                      'assets/images/login_section/png/alterra_blue_logo.png',
-                  width: 132,
-                  height: 72,
-                  alignment: Alignment.centerLeft,
-                ),
-                const SizedBox(height: AltaSpacing.space24),
-                AltaText(
-                  context: context,
-                  text: 'Kata Sandi',
-                  style: AltaTextStyle.headlineH2,
-                  color: AltaColor.darkGray,
-                ),
-                const SizedBox(height: AltaSpacing.space16),
-                AltaText(
-                  context: context,
-                  text:
-                      'Halo, Nahdy Dailamy Batewa! Silakan klik tombol di bawah ini untuk mengubah kata sandi akun Alterra kamu',
-                  style: AltaTextStyle.titleH2,
-                  color: AltaColor.darkGray,
-                  textAlign: TextAlign.left,
-                ),
-                const SizedBox(height: AltaSpacing.space24),
-                Row(
+                subtitle: Row(
                   children: [
-                    Expanded(
-                      child: AltaPrimaryButton(
-                        backgroundColor:
-                            MaterialStateProperty.all(AltaColor.tangerine),
-                        borderRadius: AltaBorderRadius.radius8,
-                        paddingVertical: AltaSpacing.space20,
-                        paddingHorizontal: AltaSpacing.space28,
-                        onPressed: () => Navigator.of(context).push(
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    const ConfirmNewPasswordPage())),
-                        child: AltaText(
-                          context: context,
-                          text: 'Ubah Kata Sandi',
-                          style: AltaTextStyle.titleH2,
-                          color: AltaColor.white,
-                        ),
+                    AltaText(
+                      context: context,
+                      text: 'to me',
+                      style: AltaTextStyle.bodyH1,
+                      color: AltaColor.darkGray,
+                    ),
+                    const SizedBox(width: AltaSpacing.space8),
+                    SvgPicture.asset(
+                        'assets/icon/login_section/svg/vert_down_icon.svg'),
+                  ],
+                ),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      splashRadius: 15,
+                      padding: EdgeInsets.zero,
+                      onPressed: () {},
+                      icon: SvgPicture.asset(
+                        'assets/icon/login_section/svg/reply_icon.svg',
+                        width: 15,
+                        height: 13,
+                      ),
+                    ),
+                    IconButton(
+                      splashRadius: 15,
+                      padding: EdgeInsets.zero,
+                      onPressed: () {},
+                      icon: SvgPicture.asset(
+                        'assets/icon/login_section/svg/more_vert_icon.svg',
+                        width: 3,
+                        height: 15,
                       ),
                     ),
                   ],
                 ),
-                const SizedBox(height: AltaSpacing.space20),
-                AltaText(
+              ),
+              const SizedBox(height: AltaSpacing.space8),
+              const AltaLogo(
+                imgPath:
+                    'assets/images/login_section/png/alterra_blue_logo.png',
+                width: 132,
+                height: 72,
+                alignment: Alignment.centerLeft,
+              ),
+              const SizedBox(height: AltaSpacing.space24),
+              AltaText(
+                context: context,
+                text: 'Kata Sandi',
+                style: AltaTextStyle.headlineH2,
+                color: AltaColor.darkGray,
+              ),
+              const SizedBox(height: AltaSpacing.space16),
+              AltaText(
+                context: context,
+                text:
+                    'Halo, Nahdy Dailamy Batewa! Silakan klik tombol di bawah ini untuk mengubah kata sandi akun Alterra kamu',
+                style: AltaTextStyle.titleH2,
+                color: AltaColor.darkGray,
+                textAlign: TextAlign.left,
+              ),
+              const SizedBox(height: AltaSpacing.space24),
+              Row(
+                children: [
+                  Expanded(
+                    child: AltaPrimaryButton(
+                      backgroundColor:
+                          MaterialStateProperty.all(AltaColor.tangerine),
+                      borderRadius: AltaBorderRadius.radius8,
+                      paddingVertical: AltaSpacing.space20,
+                      paddingHorizontal: AltaSpacing.space28,
+                      onPressed: () => Navigator.of(context).push(
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  const ConfirmNewPasswordPage())),
+                      child: AltaText(
+                        context: context,
+                        text: 'Ubah Kata Sandi',
+                        style: AltaTextStyle.titleH2,
+                        color: AltaColor.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: AltaSpacing.space20),
+              AltaText(
+                context: context,
+                text:
+                    'Jika kamu tidak melakukan pendaftaran, harap hiraukan email ini',
+                style: AltaTextStyle.bodyH1,
+                color: AltaColor.black,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: AltaSpacing.space24),
+              AltaText(
+                context: context,
+                text:
+                    'Jika anda mengalami kendala ketika klik tombol "Verifikasi Akun Alterra", klik link URL di bawah ini:',
+                style: AltaTextStyle.titleH2,
+                color: AltaColor.black,
+                textAlign: TextAlign.left,
+              ),
+              AltaTextButton(
+                onPressed: () => launch(),
+                child: AltaText(
                   context: context,
                   text:
-                      'Jika kamu tidak melakukan pendaftaran, harap hiraukan email ini',
-                  style: AltaTextStyle.bodyH1,
-                  color: AltaColor.black,
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: AltaSpacing.space24),
-                AltaText(
-                  context: context,
-                  text:
-                      'Jika anda mengalami kendala ketika klik tombol "Verifikasi Akun Alterra", klik link URL di bawah ini:',
-                  style: AltaTextStyle.titleH2,
-                  color: AltaColor.black,
+                      'https://flexiprogram-api.alterraacademy.id/api/v1/auth/email-verification/eyJpdiI6IjYxSnJKTWRHYzd1ckFEcHo1Y2VQV3c9PSIsInZhbHVlIjoiT3RKd0RSVWxqdU1GWjhHUCtGTlwvcXU4TnlvOHZCKzFOUElqaVk0NnlRTlZQWjhpS3RKRVNcLzFRK3JHWE41eVwvaCIsIm1hYyI6ImFmYmY4OGE0NmNmZDQ0N2JjM2M3OWE5NmY2NTEzYTMwMjg1YjRlNjQ3YzY5NGE2YzI3OTA5NWQyYjJkYmIwOGUifQ==',
+                  style: AltaTextStyle.hyperlinkText,
+                  color: AltaColor.darkBlue,
                   textAlign: TextAlign.left,
                 ),
-                AltaTextButton(
-                  onPressed: () => launch(),
-                  child: AltaText(
-                    context: context,
-                    text:
-                        'https://flexiprogram-api.alterraacademy.id/api/v1/auth/email-verification/eyJpdiI6IjYxSnJKTWRHYzd1ckFEcHo1Y2VQV3c9PSIsInZhbHVlIjoiT3RKd0RSVWxqdU1GWjhHUCtGTlwvcXU4TnlvOHZCKzFOUElqaVk0NnlRTlZQWjhpS3RKRVNcLzFRK3JHWE41eVwvaCIsIm1hYyI6ImFmYmY4OGE0NmNmZDQ0N2JjM2M3OWE5NmY2NTEzYTMwMjg1YjRlNjQ3YzY5NGE2YzI3OTA5NWQyYjJkYmIwOGUifQ==',
-                    style: AltaTextStyle.hyperlinkText,
-                    color: AltaColor.darkBlue,
-                    textAlign: TextAlign.left,
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
